90211a507492cbf69676c37354bf7d60e57a5a8e
Simone Carriero <simonecarriero95@hotmail.com>
2022-10-08 11:15:16 +0200

    Set routing and basic layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Sidebar } from './components';
+import {
+  BasicLayout,
+  Home,
+  About,
+  Cart,
+  Checkout,
+  Error,
+  PrivateRoute,
+  Products,
+  SingleProduct,
+} from './pages';
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={<BasicLayout />}>
+          <Route
+            index
+            element={<Home />}
+          />
+          <Route
+            path='about'
+            element={<About />}
+          />
+          <Route
+            path='cart'
+            element={<Cart />}
+          />
+          <Route
+            path='products'
+            element={<Products />}>
+            <Route
+              path=':id'
+              element={<SingleProduct />}
+            />
+          </Route>
+          <Route
+            path='checkout'
+            element={<Checkout />}
+          />
+          <Route
+            path='*'
+            element={<Error />}
+          />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
