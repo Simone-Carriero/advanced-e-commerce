c121aaf6a790f86cac454f2168836832e94aa139
Simone Carriero <simonecarriero95@hotmail.com>
2022-10-08 13:40:43 +0200

    Complete Sidebar and move to BasicLayout component

--- src/pages/BasicLayout.js
+++ src/pages/BasicLayout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Footer, Navbar } from '../components';
+import { Footer, Navbar, Sidebar } from '../components';
 
 const BasicLayout = () => {
   return (
     <>
       <Navbar />
+      <Sidebar />
       <Outlet />
       <Footer />
     </>
