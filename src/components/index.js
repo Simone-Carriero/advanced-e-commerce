6b29d6b97b0a29c6ee961c6130dacbebc0b4c8f1
Simone Carriero <simonecarriero95@hotmail.com>
2022-10-08 12:21:56 +0200

    Change name of CartButtons to NavButtons

--- src/components/index.js
+++ src/components/index.js
@@ -1,26 +1,26 @@
-import FeaturedProducts from './FeaturedProducts'
-import Navbar from './Navbar'
-import CartButtons from './CartButtons'
-import Footer from './Footer'
-import Sidebar from './Sidebar'
-import Hero from './Hero'
-import Services from './Services'
-import Contact from './Contact'
-import Loading from './Loading'
-import Error from './Error'
-import ProductImages from './ProductImages'
-import AddToCart from './AddToCart'
-import Filters from './Filters'
-import ProductList from './ProductList'
-import Sort from './Sort'
-import Stars from './Stars'
-import CartContent from './CarContent'
-import PageHero from './PageHero'
-import StripeCheckout from './StripeCheckout'
+import FeaturedProducts from './FeaturedProducts';
+import Navbar from './Navbar';
+import NavButtons from './NavButtons';
+import Footer from './Footer';
+import Sidebar from './Sidebar';
+import Hero from './Hero';
+import Services from './Services';
+import Contact from './Contact';
+import Loading from './Loading';
+import Error from './Error';
+import ProductImages from './ProductImages';
+import AddToCart from './AddToCart';
+import Filters from './Filters';
+import ProductList from './ProductList';
+import Sort from './Sort';
+import Stars from './Stars';
+import CartContent from './CarContent';
+import PageHero from './PageHero';
+import StripeCheckout from './StripeCheckout';
 export {
   FeaturedProducts,
   Navbar,
-  CartButtons,
+  NavButtons,
   Footer,
   Sidebar,
   Hero,
@@ -37,4 +37,4 @@ export {
   CartContent,
   PageHero,
   StripeCheckout,
-}
+};
